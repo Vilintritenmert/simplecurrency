9be2e828b36dfa22877f240f906657620ac72196
victor <vilintritenmert@gmail.com>
2017-10-17 07:52:09 +0300

    add checking to wrong response

--- src/SimpleCurrency.php
+++ src/SimpleCurrency.php
@@ -10,9 +10,14 @@ class SimpleCurrency
         $api_url = 'http://api.fixer.io/latest?base=usd';
         $response = @file_get_contents($api_url);
         
-        if(!$response) throw new Exception('Problem with connection');
+        if(!$response) 
+            throw new Exception('Problem with connection');
         
         $decoded_response = json_decode($response, true);
+        
+        if(!array_key_exists('rates', $decoded_response)) 
+            throw new Exception('Wrong response');
+
         $currencies = $decoded_response['rates'];
         $currencies['USD']=(float)1;
 
